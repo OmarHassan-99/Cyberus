7f7ace5d53956277f5c543b43fe633f57d09c542
Omar Hassan <114350796+OmarHassan-99@users.noreply.github.com>
2023-08-15 23:20:29 +0300

    reg.py

--- Cyberus/reg.py
+++ Cyberus/reg.py
@@ -1,9 +1,27 @@
-from flask import Flask, request, jsonify, render_template,redirect,url_for,flash
+from flask import Flask, request, jsonify, render_template,redirect,url_for,session
 import db
 Registration = Flask(__name__)
-connection = db.connect_to_database()
+Registration.secret_key = "SUPER-SECRET"
+connect = db.DBconnect()
 
-@Registration.route('/')
+@Registration.route('/register', methods = ["GET", "POST"])
+def reg():
+    if request.method == 'GET':
+        return render_template('register_team.html')
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        user = db.get_users(connect, username)
+        if user:
+            return redirect(url_for('reg_not_complete'))
+        db.add_users(connect, username, password)
+        return redirect(url_for('login'))
+
+@Registration.route('/login', methods = ["GET"])
+def login():
+    return render_template('login_team.html')
+
+@Registration.route('/', methods = ["GET"])
 def default():
     return "Hello"
 
@@ -15,9 +33,6 @@ def reg_complete():
 def reg_not_complete():
     return "Registration has not been completed"
 
-  
-  
-  
-  
 if (__name__) == 'main':
+    db.DBcreate(connect)
     Registration.run(debug = True)
